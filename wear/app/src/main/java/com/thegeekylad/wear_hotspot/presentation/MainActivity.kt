dc7b9dcf80dc651b3813ef305f970e5815ec1628
theGeekyLad <rahul.pillai03@gmail.com>
2023-10-15 14:24:42 -0700

    Added upload, download, free space

--- wear/app/src/main/java/com/thegeekylad/wear_hotspot/presentation/MainActivity.kt
+++ wear/app/src/main/java/com/thegeekylad/wear_hotspot/presentation/MainActivity.kt
@@ -8,11 +8,16 @@ package com.thegeekylad.wear_hotspot.presentation
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.ConnectivityManager
 import android.net.Uri
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.os.Environment
+import android.os.StatFs
 import android.provider.Settings
 import android.util.Log
+import android.view.WindowManager
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -21,14 +26,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import com.thegeekylad.wear_hotspot.presentation.theme.Wear_HotspotTheme
 import io.javalin.Javalin
+import io.javalin.core.util.FileUtil
+import io.javalin.http.Context
 import j2html.TagCreator.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -41,67 +55,90 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.Base64
 
+@ExperimentalUnitApi
 class MainActivity : ComponentActivity() {
+
+    var app: Javalin? = null
+    var ipAddress = mutableStateOf("")
+
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WearApp("Android")
+            Wear_HotspotTheme {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colors.background),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "File Server", fontWeight = FontWeight.Black, fontStyle = FontStyle.Italic)
+                    Text(text = ipAddress.value, fontSize = TextUnit(25f, TextUnitType.Sp))
+                    Text(text = "Port: 7070")
+                }
+            }
         }
 
-        var uploadableName: String = ""
+        // keep screen on
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
-        if (!Environment.isExternalStorageManager()) {
-            val intent = Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION, Uri.parse("package:com.thegeekylad.wear_hotspot"))
-            startActivity(intent)
-        }
+        // get ip
+        val connectivityManager = getSystemService(ConnectivityManager::class.java)
+        val linkProperties = connectivityManager.getLinkProperties(connectivityManager.activeNetwork)
+        val dhcpAddress = linkProperties?.dhcpServerAddress?.hostAddress
+        val dhcpAddressType = dhcpAddress?.substring(0, dhcpAddress.lastIndexOf("."))
+        val linkAddresses = linkProperties?.linkAddresses
+        ipAddress.value = linkAddresses?.find { linkAddress ->
+            linkAddress.address.hostAddress!!.startsWith(dhcpAddressType!!)
+        }!!.address.hostAddress!!.toString()
 
         // run server
-        val app = Javalin
-            .create(/*config*/)
-            .get("/") { ctx -> ctx.result("Hello World") }.start(7070)
+        app = Javalin
+            .create { config ->
+                config.enableCorsForAllOrigins()
+            }
+            .apply {
+                get("/") { ctx -> ctx.result("Hello World") }.start(7070)
+
+                get("/list/*") { ctx ->
+                    try {
+                        val path = ctx.path().replace("%20", " ")
+                        val indexPathStart = path.indexOf("list") + 4
+                        if (path.length > indexPathStart)
+                            serveContentForPath(ctx, path.substring(indexPathStart))
+                        else
+                            serveContentForPath(ctx)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+
+                post("/upload") { ctx ->
+                    ctx.uploadedFiles("files").forEach { uploadedFile ->
+                        try {
+                            Log.e("UPLOAD_DIR", uploadedFile.filename)
+                            FileUtil.streamToFile(uploadedFile.content, Environment.getExternalStorageDirectory().absolutePath + uploadedFile.filename)
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+                }
+            }
 
 //        TODO authenticate session
 //        app.before("/list/*") { ctx ->
 //            // runs before request to /path/*
 //            Toast.makeText(applicationContext, "Intercepted!", Toast.LENGTH_LONG).show()
 //        }
-
-        app.get("/list/*") { ctx ->
-            try {
-                val path = ctx.path().replace("%20", " ")
-                val indexPathStart = path.indexOf("list") + 5
-                if (path.length > indexPathStart)
-                    serveContentForPath(ctx, path.substring(indexPathStart))
-                else
-                    serveContentForPath(ctx)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-
-        app.post("/upload/prepare") { ctx ->
-            uploadableName = ctx.body()
-            ctx.status(200)
-        }
-
-        app.post("/upload") { ctx ->
-            try {
-                var body = ctx.body()
-                body = body.substring(body.indexOf(",") + 1)
-                body.replace("\n", "")
-                body.replace("\r", "")
-                val decodedBytes = Base64.getDecoder().decode(body)
-                val destinationFilePath = Paths.get(Environment.getExternalStorageDirectory().absolutePath, "/Download/$uploadableName")
-                Files.write(destinationFilePath, decodedBytes)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-            ctx.status(201);
-        }
     }
 
-    fun serveContentForPath(ctx: io.javalin.Context, path: String = "") {
+    override fun onPause() {
+        super.onPause()
+        app?.stop()
+    }
+
+    fun serveContentForPath(ctx: Context, path: String = "") {
         // list all files
         val potentialDir = File(Environment.getExternalStorageDirectory(), path)
 
@@ -118,18 +155,22 @@ class MainActivity : ComponentActivity() {
             head(
                 title("Watch FTP"),
                 style("" +
-                        "a {" +
+                        ".file-item {" +
                         "   text-decoration: underline;" +
+                        "   color: blue" +
                         "}" +
-                        "a:hover {" +
+                        ".file-item:hover {" +
                         "   cursor:pointer;" +
                         "}" +
-                        "")
+                        ".ml-1 {" +
+                        "   margin-left: 1rem;" +
+                        "}")
             ),
             body(
                 h1("Rahul's Pixel Watch"),
+                i(h3("Storage: ${Math.floor(getFreeSpace() * 1.0 / getTotalSpace() * 100).toInt()}% free (${getFreeSpace()} GB / ${getTotalSpace()} GB)")),
                 hr(),
-                base().withHref("http://localhost:7070/list/"),
+                base().withHref("http://$ipAddress:7070/list/"),
                 table(
                     thead(
                         tr(
@@ -141,7 +182,7 @@ class MainActivity : ComponentActivity() {
                         *files.map { file ->
                             tr(
                                 td(
-                                    a(file.name)
+                                    div(file.name)
                                         .withClass("file-item")
                                         .attr("data-file-name", file.name)
                                         .attr("style", "")
@@ -154,7 +195,20 @@ class MainActivity : ComponentActivity() {
                 hr(),
                 div(
                     span("Upload File: "),
-                    input().withType("file").withValue("Upload File").withId("input-file-upload")
+                    input()
+                        .withId("input-file-upload")
+                        .withName("files")
+                        .withType("file")
+                        .withValue("Upload File")
+                        .withCondMultiple(true),
+                    progress()
+                        .withClass("ml-1")
+                        .withId("progress-bar")
+                        .withValue("0")
+                        .withMax("100"),
+                    span()
+                        .withClass("ml-1")
+                        .withId("upload-status")
                 ),
                 hr(),
                 i(p("Made with <3 by theGeekyLad")),
@@ -163,20 +217,52 @@ class MainActivity : ComponentActivity() {
 
                 script().withSrc("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"),
                 script("" +
-                        "   function uploadFile(name, base64Data) {" +
-                        "       axios.post('http://localhost:7070/upload/prepare', name)" +
-                        "           .then(res => {" +
-                        "              axios.post('http://localhost:7070/upload', base64Data)" +
-                        "                  .then(function (res) {" +
-                        "                      alert('Upload complete!')" +
-                        "                  })" +
-                        "                  .catch(function (err) {" +
-                        "                      alert('Error: ' + err.message)" +
-                        "                  });" +
-                        "           })" +
-                        "           .catch(err => {" +
-                        "               alert('Error: Could not even share file name!')" +
-                        "           })" +
+                        "   function uploadFile() {" +
+                        "       var files = document.getElementById('input-file-upload').files;" +
+                        "       var formData = new FormData();" +
+                        "       Array.from(files).forEach(file => {" +
+                        "           console.log('file', file);" +
+                        "           const url = window.location.href;" +
+                        "           console.log('url', url);" +
+                        "           const subUrl = url.substring(" +
+                        "               url.indexOf('list') + 4" +
+                        "           );" +
+                        "           const newFile = new File(" +
+                        "               [file]," +
+                        "               subUrl + file.name," +
+                        "               {" +
+                        "                   type: file.type" +
+                        "               }" +
+                        "           );" +
+                        "           formData.append('files', newFile);" +
+                        "       });" +
+                        "       var ajax = new XMLHttpRequest();" +
+                        "       ajax.upload.addEventListener('progress', progressHandler, false);" +
+                        "       ajax.addEventListener('load', completeHandler, false);" +
+                        "       ajax.addEventListener('error', errorHandler, false);" +
+                        "       ajax.addEventListener('abort', abortHandler, false);" +
+                        "       ajax.open('POST', '/upload');" +
+                        "       ajax.send(formData);" +
+                        "   }" +
+                        "" +
+                        "   function progressHandler(event) {" +
+                        "       var percent = (event.loaded / event.total) * 100;" +
+                        "       document.getElementById('upload-status').innerHTML = 'Uploaded ' + event.loaded + ' / ' + event.total + ' bytes';" +
+                        "       document.getElementById('progress-bar').value = Math.round(percent);" +
+//                        "       document.getElementById('status').innerHTML = Math.round(percent) + '%';" +
+                        "   }" +
+                        "" +
+                        "   function completeHandler(event) {" +
+                        "       document.getElementById('upload-status').innerHTML = 'Upload complete!';" +
+//                        "       document.getElementById('progressBar').value = 0;" +
+                        "   }" +
+                        "" +
+                        "   function errorHandler(event) {" +
+                        "       document.getElementById('upload-status').innerHTML = 'Upload failed!';" +
+                        "   }" +
+                        "" +
+                        "   function abortHandler(event) {" +
+                        "       document.getElementById('upload-status').innerHTML = 'Upload aborted!';" +
                         "   }" +
                         "" +
                         "   var fileItems = document.getElementsByClassName('file-item');" +
@@ -187,50 +273,18 @@ class MainActivity : ComponentActivity() {
                         "   });" +
                         "" +
                         "   document.getElementById('input-file-upload').addEventListener('change', (e) => {" +
-                        "       if (e.target.files.length === 0) return;" +
-                        "       const file = e.target.files[0];" +
-                        "       const reader = new FileReader();" +
-                        "       reader.addEventListener('load', () => {" +
-                        "           uploadFile(file.name, reader.result);" +
-                        "       }, false);" +
-                        "       reader.readAsDataURL(file);" +
+                        "       uploadFile();" +
                         "   }, false);" +
                         "").withType("text/javascript")
             )
         ).render())
     }
-}
 
-@Composable
-fun WearApp(greetingName: String) {
-    Wear_HotspotTheme {
-        /* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
-         * version of LazyColumn for wear devices with some added features. For more information,
-         * see d.android.com/wear/compose.
-         */
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background),
-            verticalArrangement = Arrangement.Center
-        ) {
-            Greeting(greetingName = greetingName)
-        }
+    fun getFreeSpace(): Long {
+        return StatFs(Environment.getExternalStorageDirectory().absolutePath).freeBytes / 1024 / 1024 / 1024
     }
-}
 
-@Composable
-fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = "Running nano server @ 7070"
-    )
-}
-
-@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    WearApp("Preview Android")
+    fun getTotalSpace(): Long {
+        return StatFs(Environment.getExternalStorageDirectory().absolutePath).totalBytes / 1024 / 1024 / 1024
+    }
 }
